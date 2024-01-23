6cce1fd452b337ab299bee6dbae13d777fea14d8
Anavi-K <anavikothari@outlook.com>
2024-01-23 11:14:36 +0530

    updated

--- pronote/src/component/Editor.jsx
+++ pronote/src/component/Editor.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Editor.css'; // Import your CSS file for styling
+import './Editor.css';
 
 class Editor extends Component {
     constructor(props) {
